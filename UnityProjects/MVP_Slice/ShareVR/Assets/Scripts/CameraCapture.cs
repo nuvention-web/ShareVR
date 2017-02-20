6ede8a590796ac03308c7c0f6c9e4f11ff930c83
Chen Chen <chenchen.bme@gmail.com>
2017-02-20 15:20:58 +0800

    MVP_Slice Project Initial Commit (Ver 1.0)
    [Version 1.0] Add MVP_Slice Unity project to our repository. This
    version implemented basic functions including SteamVR support, visible
    spectator camera. automatic spectator camera follow, in-game real-time
    camera live feed and in-game interactive UI. Tested on PC without
    SteamVR. Need to test with SteamVR on HTC Vive.

---
+++ UnityProjects/MVP_Slice/ShareVR/Assets/Scripts/CameraCapture.cs
@@ -0,0 +1,45 @@
+﻿//======= Copyright (c) NUVention TeamH ShareVR ===============
+//
+// Purpose: Controls all the capture events and methods from the spectator camera
+// Version: 1.0
+// Date: 2/20/2017
+// Revision History:  v1.0 - Chen Chen - Created class for MVP Slice
+// 
+//=============================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCapture : MonoBehaviour
+{
+	public Camera targetCam;
+	public RenderTexture rtex;
+
+	CameraFollow camFollow;
+	bool isRendering = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		camFollow = gameObject.GetComponent <CameraFollow> ();
+	}
+
+	void Update ()
+	{
+		if (camFollow.GetCamStatus () && !isRendering) {
+			StartCoroutine (CamRenderToTexture (targetCam));
+			isRendering = true;
+		} else if (!camFollow.GetCamStatus ()) {
+			StopCoroutine (CamRenderToTexture (targetCam));
+			isRendering = false;
+		}
+
+	}
+
+	private IEnumerator CamRenderToTexture (Camera cam)
+	{
+		cam.targetTexture = rtex;
+		cam.Render ();
+		yield return new WaitForSeconds (1.0f);
+	}
+}
