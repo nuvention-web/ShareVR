a6a8014cd2e8e18a6a47da1629407656871fc598
Chen Chen <chenchen.bme@gmail.com>
2017-05-04 18:35:17 -0500

    Add VisualStudioProjects for DLL development

---
+++ VisualStudioProjects/ShareVR/ShareVR/LogManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace ShareVR.Utils
+{
+    using MetaData = Dictionary<string, string>;
+
+    static class LogManager
+    {
+        private static MetaData metadata;
+
+        public static MetaData Metadata { get => metadata; set => metadata = value; }
+    }
+}
